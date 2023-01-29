16a42e8768fce5acb88e227244df17c353a8ea87
Mayron Silva <mayronsilva147@gmail.com>
2023-01-29 11:54:19 -0300

    commit do programa par e impar

---
+++ Repetição e arrays/src/ParEImpar.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ParEImpar {
+  public static void main(String[] args) {
+    try (Scanner scan = new Scanner(System.in)) {
+      int quantNumeros;
+      int numero;
+      int quantPares = 0, quantImpares = 0;
+
+      System.out.println("Quantidade de números: ");
+      quantNumeros = scan.nextInt();
+
+      int count = 0;
+      do {
+        System.out.println("Numero: ");
+        numero = scan.nextInt();
+
+        if (numero % 2 == 0)
+          quantPares++;
+
+        else
+          quantImpares++;
+
+        count++;
+
+      } while (count < quantNumeros);
+
+      System.out.println("Quantidade Par: " + quantPares);
+      System.out.println("Quantidade Impar: " + quantImpares);
+    }
+  }
+
+}
